3dd0e59cb64bbcd7633c987be4c23e07008aa54b
Lucas Rodriguez <lrodriguez@lexim.solutions.com>
2025-09-23 15:46:03 -0300

    Commit 23/09

--- burro/src/app/core/models/usuario.model.ts
+++ burro/src/app/core/models/usuario.model.ts
@@ -1,1 +1,25 @@
-// placeholder
+export enum perfil {
+    Duenio = 'Duenio',
+    Supervisor = 'Supervisor',
+    Empleados = '', //Hacer otro enum para tipos de empleados o verlo de otra manera?
+    ClienteRegistrado = 'ClienteRegistrado',
+    ClienteAnonimo = 'ClienteAnonimo'
+}
+
+export class Usuario {
+
+    public nombre : string;
+    public apellido : string;
+    public numeroDocumento : number;
+    public correo : string;
+    public perfil : perfil;
+
+    constructor(nombre: string, apellido: string, numeroDocumento : number, correo: string, perfil: perfil){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDocumento = numeroDocumento;
+        this.correo = correo;
+        this.perfil = perfil
+    }
+
+}
