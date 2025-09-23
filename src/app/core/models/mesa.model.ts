88845c33e6089a31f3db8b319a6b1e85e0d7c388
Lucas Rodriguez <lrodriguez@lexim.solutions.com>
2025-09-23 16:27:45 -0300

    Commit 23/09

--- src/app/core/models/mesa.model.ts
+++ src/app/core/models/mesa.model.ts
@@ -1,1 +1,23 @@
-//placeholder
+export enum tipoMesa {
+    Estandar = 'Estandar',
+    VIP = 'VIP',
+    MovilidadReducida = 'MovilidadReducida'
+}
+
+export class Mesa {
+
+    public id : number;
+    public estado : boolean;
+    public tipo : tipoMesa;
+    public cantidadComensales : number;
+    public numeroMesa : number;
+
+    constructor(id: number, estado: boolean, tipo : tipoMesa, cantidadComensales: number, numeroMesa: number){
+        this.id = id;
+        this.estado = estado;
+        this.tipo = tipo;
+        this.cantidadComensales = cantidadComensales;
+        this.numeroMesa = numeroMesa
+    }
+
+}
