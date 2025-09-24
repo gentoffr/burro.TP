b506ccb5e5f0416820e0f0f6328190b97d832a35
gentoffr <frangento@hotmail.com>
2025-09-24 18:08:30 -0300

    ignore

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { 
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  MenuController
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [ IonicModule, FormsModule ],
+  imports: [ 
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+  ],
 })
 export class LoginComponent {
   email:string = "";
 
   contrasena:string = "";
-
+  constructor(private menuCtrl: MenuController) { }
   ionViewWillEnter() {
     this.email = "";
     this.contrasena = "";
   }
-
+  
 }
