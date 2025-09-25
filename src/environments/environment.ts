1afa6ed24c03dd8ce934953fe3ed1b2e27b196c8
gentoffr <frangento@hotmail.com>
2025-09-25 17:47:24 -0300

    ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://ddkzionubeqzjtopkytq.supabase.co',
-  supabaseKey: 'sb_publishable_yRBCB2oYVptpbvKivwUo8Q_w4J3mRAm'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRka3ppb251YmVxemp0b3BreXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NDYxNjMsImV4cCI6MjA3NDIyMjE2M30.fXScUizc7pBD8a7AYpkoQI8VaTox81iPyS14SbAXyes'
 };
 
 /*
