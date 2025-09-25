6b68eaba4eb6dc0edf1f5dcfe013064ec3bc4159
gentoffr <frangento@hotmail.com>
2025-09-25 17:46:02 -0300

    dashboard sencillo, refleja sesion actual

--- src/app/features/dashboards/dash-duenio/dash-duenio.component.ts
+++ src/app/features/dashboards/dash-duenio/dash-duenio.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonAvatar, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { UsuarioService } from '../../../core/services/usuario.service';
+import { Usuario } from '../../../core/models/usuario.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dash-duenio',
   templateUrl: './dash-duenio.component.html',
   styleUrls: ['./dash-duenio.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonButtons, IonMenuButton]
+  imports: [CommonModule, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonAvatar, IonLabel, IonItem]
 })
-export class DashDuenioComponent  {}
+export class DashDuenioComponent implements OnInit {
+  currentUser$: Observable<Usuario | null>;
+
+  constructor(private usuarioService: UsuarioService) {
+    this.currentUser$ = this.usuarioService.currentUser$;
+  }
+
+  ngOnInit() {
+    // Opcional: log para debug
+    this.currentUser$.subscribe(user => {
+      console.log('Usuario actual en dashboard:', user);
+    });
+  }
+}
